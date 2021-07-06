75dc1a6180bbef35e9c1124afc1766dc160fb007
edward frempong <eddysoftwarengineer@gmail.com>
2021-07-06 14:05:19 +0000

    config and thunk with redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,21 @@
 import { createStore } from 'redux';
+import {createStore, compose, applyMiddleware } from 'redux';
 import userReducer from '../reducers/usersReducer';
+import {getFirebase, reduxReactFirebase} from 'react-redux-firebase';
+import { getFirestore, reduxFirestore } from 'redux-firestore';
+import firebase from '../firebase/config';
+
+import thunk from 'redux-thunk';
 
 
-const store = createStore(userReducer);
+
+const store = createStore(
+    userReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+        reduxReactFirebase(firebase),
+        reduxFirestore(firestore)
+    )    
+    );
 
 export default store;
