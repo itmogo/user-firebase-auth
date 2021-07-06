c479336bdf6ce2788daa5f11b4e8ff5c8f76e19e
edward frempong <eddysoftwarengineer@gmail.com>
2021-07-06 17:03:13 +0000

    add user to firebase and get users from firebase

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,8 +1,44 @@
+export function getAllUsers(){
+  return (dispatch, state, {getFirestore}) => {
+    const db = getFirestore();
+    db.collection('users')
+     .get()
+     .then((results) => { 
+       let users = [];
+       results.forEach((doc) => {
+         //console.log(doc.id, doc.data());
+         users.push(doc.data());
+       });
+
+     dispatch({
+       type: 'ADD_ALL_USERS',
+       payload: users,
+     });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+     
+  };
+}
+
+
 export function addUserAction(user) {
-    return {
-      type: 'ADD_USER',
-      payload: user,
-    };
+    return (dispatch,state, {getFirestore}) =>{
+        const db = getFirestore();
+        db.collection('users')
+        .add(user)
+        .then(() => {
+          //send the data to the store
+          dispatch({
+            type: 'ADD_USER',
+            payload: user,
+          });
+        })
+        .catch ((err) => {
+          console.log(err);
+        });
+       };
   }
   
   export function deleteUserAction(id) {
