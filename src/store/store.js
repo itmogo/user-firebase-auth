93813f6190a4dc37d955cda3ee8592db13fb9ba7
edward frempong <eddysoftwarengineer@gmail.com>
2021-07-07 16:58:25 +0000

    corrected userreducer to usersreducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 //import { createStore } from 'redux';
 import {createStore, compose, applyMiddleware } from 'redux';
-import userReducer from '../reducers/usersReducer';
+import usersReducer from '../reducers/usersReducer';
 import {getFirebase, reduxReactFirebase} from 'react-redux-firebase';
 import { getFirestore, reduxFirestore } from 'redux-firestore';
 import firebase from '../firebase/config';
@@ -10,7 +10,8 @@ import thunk from 'redux-thunk';
 
 
 const store = createStore(
-    userReducer,
+    usersReducer,
+    
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxReactFirebase(firebase),
