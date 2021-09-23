5f45883f765de34bdb4ed4bbf453d8ad89ecf9f5
edward frempong <eddysoftwarengineer@gmail.com>
2021-09-23 04:22:34 +0000

    added login code

---
+++ src/pages/login.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import {login} from '../actions/authActions';
+import { connect } from "react-redux";
+import "./form.css";
+import { Button, Form } from "react-bootstrap";
+
+//user sign up component
+function Login(props) {
+  // default user state is an object with empty string as value
+  const [credentials, setCredentials] = useState({  
+    email: "",
+    password: "",    
+  });
+
+  //a function that get called anytime an input field changes
+  function handleOnChange(event) {
+    //event.target.name hold the name of the input that changed
+    //event.target.value hold the new value of the input field that changed
+
+    //we update the user state with the new value
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  //this function will be called the the create user button is clicked on
+  function handleSubmit() {
+    //we call addUser function passed to this user form component
+    //as a prop from the App component
+   // let userId = 10000 + Math.random() * 10000000;
+   // let user = { ...state, id: userId };
+    props.login(credentails.email, credentials.password);
+  }
+
+  return (
+    <div>
+      <form>
+        <div>
+         <Form.Group controlId="formBasicEmail">
+          <Form.Label>Login</Form.Label>
+          <Form.Control 
+          type="email" 
+          name="email"
+          onChange={handleOnChange} //we setup onchange to call our handle onchange function
+          required />
+          </Form.Group>
+
+        </div>
+               <br />
+
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+          <br />
+          <Form.Control
+            type="password"
+            name="password"           
+            onChange={handleOnChange} //we setup onchange to call our handle onchange function
+           
+            required />
+        </Form.Group>
+        <br /><br />
+
+        <div>
+          {/* the create user button call the handleSubmit functon when clicked */}
+          <Button type="button" variant="primary" onClick={handleSubmit}>
+            <span>Log In</span>
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+const mapDispatchToProps = {login};
+export default connect(null, mapDispatchToProps)(Login);
