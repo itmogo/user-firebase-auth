14ac1d436e4081e9615a94bcda50e943af4bbd2c
edward frempong <eddysoftwarengineer@gmail.com>
2021-09-23 04:45:26 +0000

    added login code in authactions file

---
+++ src/actions/authActions.js
@@ -0,0 +1,33 @@
+export function signup(email, password){
+    return async (dispatch, state, {getFirebase})=>{
+        const firebase =getFirebase();
+
+        try{
+            const user = await firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password);
+
+            console.log(user);
+        } catch (error){
+            console.log(error);
+        }
+    };
+}
+
+
+
+export function login (email, password){
+    return async (dispatch, state, {getFirebase}) =>{
+        const firebase = getFirebase();
+
+        try{
+            const userCredential = await firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password);
+            console.log(userCredential)
+        }
+        catch{e }{
+            console.log(e)
+        }
+    };
+}
